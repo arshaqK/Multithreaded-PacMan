2abf76b20e2355fd6e00e69e4c524f0894eb2a6f
Syed Arshaq Hussain Kirmani <88348611+arshaqK@users.noreply.github.com>
2024-05-07 22:37:18 +0500

    Update main.c
    Complete board interface with coins

--- main.c
+++ main.c
@@ -1,35 +1,38 @@
 #include <GL/glut.h>
 #include <stdio.h>
+#include <math.h>
+#include <GL/gl.h>
 
-#define WIDTH 25
-#define HEIGHT 25
-#define CELL 30
+int WIDTH = 25;
+int HEIGHT = 25;
+int cellSize = 30;
+float PI = 3.14159265358979323846;
 
 char board[25][25] = {
         "#########################",
         "#.......... # ..........#",
-        "#####.    #####     #####",
+        "#####.....#####.....#####",
         "#.......................#",
-        "# ####.####.#.####.####.#",
-        "#.   #.#    #   .#.#    #",
+        "#.####.####.#.####.####.#",
+        "#....#.#....#....#.#....#",
         "#.####.####.#.####.####.#",
         "#.......................#",
-        "#####.#  ####### .# #####",
-        "#    .#     #     #     #",
-        "# # #.# ###- -### # # # #",
-        "# # #.# #  P    #.# # # #",
-        "# # #.# #  - -  # # # # #",
-        "# # #.# ######### # # # #",
-        "#   #.#           # #   #",
-        "#####.#  ######## # #####",
-        "#.........   .. #.. ....#",
-        "# ###### ###### #. ######",
-        "# ..    .....        .  #",
-        "# # # #.#   #   # #.#.# #",
-        "# # # #.###   ### #.#.# #",
-        "# #.# #. .... ....#.# # #",
-        "#.# # #####.#.##### # # #",
-        "#...#...............#. .#",
+        "#####.#..#######..#.#####",
+        "#.....#.....#.....#.....#",
+        "#.#.#.#.###- -###.#.#.#.#",
+        "#.#.#.#.#  P    #.#.#.#.#",
+        "#.#.#.#.#  - -  #.#.#.#.#",
+        "#.#.#.#.#########.#.#.#.#",
+        "#...#.#...........#.#...#",
+        "#####.#.#########.#.#####",
+        "#...............#.......#",
+        "#.######.######.#..######",
+        "#.......................#",
+        "#.#.#.#.#...#...#.#.#.#.#",
+        "#.#.#.#.###...###.#.#.#.#",
+        "#.#.#.#...........#.#.#.#",
+        "#.#.#.#####.#.##### #.#.#",
+        "#...#...............#...#",
         "#########################",
        };
 
@@ -42,9 +45,9 @@ void display(){
   	glColor3f(0.0f, 0.0f, 0.0f);
 	glBegin(GL_QUADS);
 	glVertex2i(0, 0);
-	glVertex2i(WIDTH * CELL, 0);
-	glVertex2i(WIDTH * CELL, HEIGHT * CELL);
-	glVertex2i(0, HEIGHT * CELL);
+	glVertex2i(WIDTH * cellSize, 0);
+	glVertex2i(WIDTH * cellSize, HEIGHT * cellSize);
+	glVertex2i(0, HEIGHT * cellSize);
 	glEnd();
 
   
@@ -53,15 +56,39 @@ void display(){
        for (int i = 0; i < HEIGHT; i++) {
        	for (int x = 0; x < WIDTH; x++) {
 			if (board[i][x] == '#') {
-			  	int left = x * CELL;
-				int top = (HEIGHT - i) * CELL; 
+			  	int left = x * cellSize;
+				int top = (HEIGHT - i) * cellSize; 
 
 				glBegin(GL_QUADS);
 				glVertex2i(left, top);
-				glVertex2i(left + CELL, top);
-				glVertex2i(left + CELL, top - CELL);
-				glVertex2i(left, top - CELL);
+				glVertex2i(left + cellSize, top);
+				glVertex2i(left + cellSize, top - cellSize);
+				glVertex2i(left, top - cellSize);
 				glEnd();
+			}
+			
+		}
+	}
+	
+	
+	glColor3f(1.3f, 1.3f, 0.2f); 
+	float foodRadius = cellSize / 7.0f;
+
+	for (int i = 0; i < HEIGHT; i++) {
+		for (int x = 0; x < WIDTH; x++) {	
+			if (board[i][x] == '.') {				    
+				float centerX = (x * cellSize) + (cellSize / 2.0f);
+				float centerY = (HEIGHT - 1 - i) * cellSize + (cellSize / 2.0f); // Corrected for top-down display
+				int numVertices = 30; // Adjust for circle smoothness
+
+				glBegin(GL_POLYGON);
+				for (int i = 0; i < numVertices; i++) {
+				    float angle = 2 * PI * i / numVertices;
+				    float xPos = centerX + foodRadius * cos(angle);
+				    float yPos = centerY + foodRadius * sin(angle);
+				    glVertex2f(xPos, yPos);
+				}
+				glEnd();		
 			}
 		}
 	}
@@ -74,7 +101,7 @@ void display(){
 void reshape(int width, int height) {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(0, WIDTH * CELL, 0, HEIGHT * CELL);
+	gluOrtho2D(0, WIDTH * cellSize, 0, HEIGHT * cellSize);
 	glMatrixMode(GL_MODELVIEW);
 	glViewport(0, 0, width, height);
 }
@@ -84,7 +111,7 @@ int main(int argc, char** argv) {
 	
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(WIDTH*CELL, HEIGHT*CELL);
+	glutInitWindowSize(WIDTH*cellSize, HEIGHT*cellSize);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Multithreaded PacMan");
 	glutDisplayFunc(display);
