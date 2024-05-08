2515f79de7da966f485bde3ce73fea16f1bbee36
zainnawaz22 <i221085@nu.edu.pk>
2024-05-08 18:54:24 +0500

    chore: Refactor keyboard controls and image loading in main.c

--- main.c
+++ main.c
@@ -18,23 +18,7 @@ float PI = 3.14159265358979323846;
 
 GLuint texture; // Add this line at the top of your file
 
-void initOpenGL(){
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0, 300, 300, 0);
-    glMatrixMode(GL_MODELVIEW);
 
-    
-    int width, height;
-    unsigned char* image = SOIL_load_image("pacman.png", &width, &height, 0, SOIL_LOAD_RGB);
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    SOIL_free_image_data(image);
-}
 
 char board[25][25] = {
 	"#########################",
@@ -138,24 +122,7 @@ void* display(void *arg){
 
 }
 
-void keyboard(int key, int x, int y) {
-    switch (key) {
-        case GLUT_KEY_RIGHT:
-            playerX += 0.1f;
-            break;
-        case GLUT_KEY_LEFT:
-            playerX -= 0.1f;
-            break;
-        case GLUT_KEY_DOWN:
-            playerY -= 0.1f;
-            break;
-        case GLUT_KEY_UP:
-            playerY += 0.1f;
-            break;
-    }
-    
-    glutPostRedisplay();
-}
+
 
 
 void reshape(int width, int height) {
@@ -179,23 +146,40 @@ void *gameEngine(void *arg){
 }
 
 void keyboard(int key, int x, int y) {
-	switch (key) {
-		case GLUT_KEY_RIGHT:
-			x += 10;
-			break;
-		case GLUT_KEY_LEFT:
-			x -= 10;
-			break;
-		case GLUT_KEY_DOWN:
-			y -= 10;
-			break;
-		case GLUT_KEY_UP:
-			y += 10;
-			break;
+	// switch (key) {
+	// 	case GLUT_KEY_RIGHT:
+	// 		x += 10;
+	// 		break;
+	// 	case GLUT_KEY_LEFT:
+	// 		x -= 10;
+	// 		break;
+	// 	case GLUT_KEY_DOWN:
+	// 		y -= 10;
+	// 		break;
+	// 	case GLUT_KEY_UP:
+	// 		y += 10;
+	// 		break;
 		
-	}
+	// }
+
+	    switch (key) {
+        case GLUT_KEY_RIGHT:
+            playerX += 0.1f;
+            break;
+        case GLUT_KEY_LEFT:
+            playerX -= 0.1f;
+            break;
+        case GLUT_KEY_DOWN:
+            playerY -= 0.1f;
+            break;
+        case GLUT_KEY_UP:
+            playerY += 0.1f;
+            break;
+    }
+    
+    glutPostRedisplay();
 	
-	glutPostRedisplay();
+	
 }
 
 void initOpenGL(){
@@ -204,6 +188,21 @@ void initOpenGL(){
 	glLoadIdentity();
 	gluOrtho2D(0, 300, 300, 0);
 	glMatrixMode(GL_MODELVIEW);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, 300, 300, 0);
+    glMatrixMode(GL_MODELVIEW);
+
+    
+    int width, height;
+    unsigned char* image = SOIL_load_image("pacman.tfif", &width, &height, 0, SOIL_LOAD_RGB);
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    SOIL_free_image_data(image);
 }
 
 int main(int argc, char** argv) {
@@ -222,7 +221,7 @@ int main(int argc, char** argv) {
 	glutReshapeFunc(reshape);
 	glutSpecialFunc(keyboard);
 	glutMainLoop();
-	
+
 	
 	
 	//pthread_join(game_engine, NULL);
